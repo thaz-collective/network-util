16b337d45414354187a580610303fa61d9e0a77e
Tim Hazlett <timhazlett2016@gmail.com>
2026-08-13 20:27:42 -0500

    WIP - standard schema export

--- src/fetch/index.ts
+++ src/fetch/index.ts
@@ -6,3 +6,5 @@ export * from './contract';
 
 export type { FetchClient, CreateFetchClientOptions, FetchClientRequestOptions } from './client';
 export { createFetchClient } from './client';
+
+export type * from './standard-schema';
